bbb803c0c0bdc28027d753f1a35f072c8c9bc3cd
XernPLL <xernpl@gmail.com>
2019-12-14 22:16:05 +0100

    part 3 done

--- util.py
+++ util.py
@@ -7,52 +7,6 @@ from torch.autograd import Variable
 import numpy as np
 import cv2
 
-def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = True):
-    batch_size = prediction.size(0)
-    stride = inp_dim // prediction.size(2)
-    grid_size = inp_dim // stride
-    bbox_attrs = 5 + num_classes
-    num_anchors = len(anchors)
-
-    prediction = prediction.view(batch_size, bbox_attrs*num_anchors, grid_size*grid_size)
-    prediction = prediction.transpose(1,2).contiguous()
-    prediction = prediction.view(batch_size, grid_size*grid_size*num_anchors, bbox_attrs)
-
-
-
-    anchors = [(a[0]/stride, a[1]/stride) for a in anchors]
-
-    prediction[:, :, 0] = torch.sigmoid(prediction[:, :, 0])
-    prediction[:, :, 1] = torch.sigmoid(prediction[:, :, 1])
-    prediction[:, :, 4] = torch.sigmoid(prediction[:, :, 4])
-
-    grid = np.arange(grid_size)
-    a, b = np.meshgrid(grid,grid)
-
-    x_offset = torch.FloatTensor(a).view(-1,1)
-    y_offset = torch.FloatTensor(b).view(-1, 1)
-
-    if CUDA:
-        x_offset = x_offset.cuda()
-        y_offset = y_offset.cuda()
-
-    x_y_offset = torch.cat((x_offset, y_offset), 1).repeat(1, num_anchors).view(-1, 2).unsqueeze(0)
-
-    prediction[:, :, :2] += x_y_offset
-
-    anchors = torch.FloatTensor(anchors)
-
-    if CUDA:
-        anchors = anchors.cuda()
-
-    anchors = anchors.repeat(grid_size * grid_size, 1).unsqueeze(0)
-    prediction[:, :, 2:4] = torch.exp(prediction[:, :, 2:4]) * anchors
-
-    prediction[:, :, 5: 5 + num_classes] = torch.sigmoid((prediction[:, :, 5: 5 + num_classes]))
-
-    prediction[:, :, :4] *= stride
-
-    return prediction
 
 def unique(tensor):
     tensor_np = tensor.cpu().numpy()
@@ -92,7 +46,56 @@ def bbox_iou(box1, box2):
 
     return iou
 
-def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
+
+def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA=True):
+    batch_size = prediction.size(0)
+    stride = inp_dim // prediction.size(2)
+    grid_size = inp_dim // stride
+    bbox_attrs = 5 + num_classes
+    num_anchors = len(anchors)
+
+    prediction = prediction.view(batch_size, bbox_attrs * num_anchors, grid_size * grid_size)
+    prediction = prediction.transpose(1, 2).contiguous()
+    prediction = prediction.view(batch_size, grid_size * grid_size * num_anchors, bbox_attrs)
+    anchors = [(a[0] / stride, a[1] / stride) for a in anchors]
+
+    # Sigmoid the  centre_X, centre_Y. and object confidencce
+    prediction[:, :, 0] = torch.sigmoid(prediction[:, :, 0])
+    prediction[:, :, 1] = torch.sigmoid(prediction[:, :, 1])
+    prediction[:, :, 4] = torch.sigmoid(prediction[:, :, 4])
+
+    # Add the center offsets
+    grid = np.arange(grid_size)
+    a, b = np.meshgrid(grid, grid)
+
+    x_offset = torch.FloatTensor(a).view(-1, 1)
+    y_offset = torch.FloatTensor(b).view(-1, 1)
+
+    if CUDA:
+        x_offset = x_offset.cuda()
+        y_offset = y_offset.cuda()
+
+    x_y_offset = torch.cat((x_offset, y_offset), 1).repeat(1, num_anchors).view(-1, 2).unsqueeze(0)
+
+    prediction[:, :, :2] += x_y_offset
+
+    # log space transform height and the width
+    anchors = torch.FloatTensor(anchors)
+
+    if CUDA:
+        anchors = anchors.cuda()
+
+    anchors = anchors.repeat(grid_size * grid_size, 1).unsqueeze(0)
+    prediction[:, :, 2:4] = torch.exp(prediction[:, :, 2:4]) * anchors
+
+    prediction[:, :, 5: 5 + num_classes] = torch.sigmoid((prediction[:, :, 5: 5 + num_classes]))
+
+    prediction[:, :, :4] *= stride
+
+    return prediction
+
+
+def write_results(prediction, confidence, num_classes, nms_conf=0.4):
     conf_mask = (prediction[:, :, 4] > confidence).float().unsqueeze(2)
     prediction = prediction * conf_mask
 
@@ -126,12 +129,14 @@ def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
 
         if image_pred_.shape[0] == 0:
             continue
+        #
 
         # Get the various classes detected in the image
         img_classes = unique(image_pred_[:, -1])  # -1 index holds the class index
 
         for cls in img_classes:
             # perform NMS
+
             # get the detections with one particular class
             cls_mask = image_pred_ * (image_pred_[:, -1] == cls).float().unsqueeze(1)
             class_mask_ind = torch.nonzero(cls_mask[:, -2]).squeeze()
@@ -162,8 +167,8 @@ def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
                 non_zero_ind = torch.nonzero(image_pred_class[:, 4]).squeeze()
                 image_pred_class = image_pred_class[non_zero_ind].view(-1, 7)
 
-            batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)
-            # Repeat the batch_id for as many detections of the class cls in the image
+            batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(
+                ind)  # Repeat the batch_id for as many detections of the class cls in the image
             seq = batch_ind, image_pred_class
 
             if not write:
@@ -177,11 +182,6 @@ def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
         return output
     except:
         return 0
-
-def load_classes(namesfile):
-    fp = open(namesfile, "r")
-    names = fp.read().split("\n")[:-1]
-    return names
 
 
 def letterbox_image(img, inp_dim):
@@ -205,8 +205,13 @@ def prep_image(img, inp_dim):
 
     Returns a Variable
     """
-
-    img = cv2.resize(img, (inp_dim, inp_dim))
+    img = (letterbox_image(img, (inp_dim, inp_dim)))
     img = img[:, :, ::-1].transpose((2, 0, 1)).copy()
     img = torch.from_numpy(img).float().div(255.0).unsqueeze(0)
     return img
+
+
+def load_classes(namesfile):
+    fp = open(namesfile, "r")
+    names = fp.read().split("\n")[:-1]
+    return names
